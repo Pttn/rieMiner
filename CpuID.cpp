faa0735c3ba60fba47dbcb1f31e328f9e9ff5ee2
Mike Bell <mdb036@gmail.com>
2018-12-01 19:49:49 +0100

    Workaround missing CPUID functions

--- CpuID.cpp
+++ CpuID.cpp
@@ -15,7 +15,13 @@ CpuID::CpuID() {
 		__get_cpuid(1, &eax, &ebx, &ecx, &edx);
 		_avx = (ecx & (1 << 28)) != 0;
 
-		__get_cpuid_count(7, 0, &eax, &ebx, &ecx, &edx);
+		// Must do this with inline assembly as __get_cpuid is unreliable for level 7
+		// and __get_cpuid_count is not always available.
+		//__get_cpuid_count(7, 0, &eax, &ebx, &ecx, &edx);
+		uint32_t level(7), zero(0);
+		asm ("cpuid\n\t"
+		    : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
+		    : "0"(level), "2"(zero));
 		_avx2 = (ebx & (1 << 5)) != 0;
 		_avx512 = (ebx & (1 << 16)) != 0;
 	}
