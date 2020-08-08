e45e17b44b9d221c02b0b2ba40bcf1ad507ef0ca
Pttn <28868425+Pttn@users.noreply.github.com>
2020-08-08 05:18:05 +0200

    Remove check in "Pendings" for slight performance improvement
    Signed-off-by: Pttn <28868425+Pttn@users.noreply.github.com>

--- Miner.hpp
+++ Miner.hpp
@@ -116,17 +116,8 @@ class Miner {
 	void _addToPending(uint8_t *sieve, std::array<uint32_t, PENDING_SIZE> &pending, uint64_t &pos, uint32_t ent) {
 		__builtin_prefetch(&(sieve[ent >> 3]));
 		uint32_t old(pending[pos]);
-		if (old != 0) {
-			// assert(old < _parameters.sieveSize);
-			if (old >= _parameters.sieveSize) {
-				std::cerr << "_addToPending: old = " << old << " is bigger than _parameters.sieveSize = " << _parameters.sieveSize << ", which should never happen!" << std::endl;
-				std::cout << "This may happen in an unstable or faulty computer. Please check your hardware or CPU/RAM frequency/voltage settings." << std::endl;
-				std::cout << "If you just worked on the code, you likely broke something." << std::endl;
-				std::cout << "Temporarily changing old to dummy value of " << _parameters.sieveSize - 1 << " to allow mining to continue." << std::endl;
-				old = _parameters.sieveSize - 1;
-			}
+		if (old != 0)
 			sieve[old >> 3] |= (1 << (old & 7));
-		}
 		pending[pos] = ent;
 		pos++;
 		pos &= PENDING_SIZE - 1;
@@ -142,10 +133,8 @@ class Miner {
 	void _termPending(uint8_t *sieve, std::array<uint32_t, PENDING_SIZE> &pending) {
 		for (uint64_t i(0) ; i < PENDING_SIZE ; i++) {
 			const uint32_t old(pending[i]);
-			if (old != 0) {
-				assert(old < _parameters.sieveSize);
+			if (old != 0)
 				sieve[old >> 3] |= (1 << (old & 7));
-			}
 		}
 	}
 	
