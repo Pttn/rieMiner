1bb6e76d76960c5b33102010fc95272ac64bdd3d
Pttn <28868425+Pttn@users.noreply.github.com>
2023-10-13 16:16:59 +0200

    Bech32M Payout Address Support
    Signed-off-by: Pttn <28868425+Pttn@users.noreply.github.com>

--- tools.cpp
+++ tools.cpp
@@ -1,4 +1,4 @@
-// (c) 2018-2022 Pttn (https://riecoin.dev/en/rieMiner)
+// (c) 2018-2023 Pttn (https://riecoin.dev/en/rieMiner)
 // (c) 2018 Michael Bell/Rockhawk (CPUID tools)
 
 #include "tools.hpp"
@@ -103,7 +103,7 @@ std::vector<uint8_t> bech32ToScriptPubKey(const std::string &address) {
 	}
 	std::vector<uint8_t> expHrpData(expandHrp(addrHrp));
 	expHrpData.insert(expHrpData.end(), v5.begin(), v5.end());
-	if (bech32Polymod(expHrpData) != 1)
+	if (bech32Polymod(expHrpData) != (address[delimiterPos + 1] == 'q' ? 1 : 0x2bc830a3))
 		return {};
 	std::vector<uint8_t> spk(v5ToV8(std::vector<uint8_t>(v5.begin() + 1, v5.end() - 6)));
 	if ((spk.size() == 0 && addrData.size() != 6) || v5[0] > 16)
